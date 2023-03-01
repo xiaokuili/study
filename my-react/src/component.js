7eebbc7ae7807c7426e220131b7e754ee946bbaa
yin1991 <84140478+xiaokuili@users.noreply.github.com>
2023-03-01 06:01:13 +0000

    父向子传递数据

--- my-react/src/component.js
+++ my-react/src/component.js
@@ -1,20 +1,24 @@
-import React from 'react';
+import React from "react";
 
 function Child() {
-    return (
-        <div>
-            <h2>child</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2>child</h2>
+    </div>
+  );
 }
 
+function Name(props) {
+  return <div>name is {props.name}</div>;
+}
 
 export default function Fathter(props) {
-    return (
-        <div>
-        <h1>father</h1>
-            {props.children}
-        </div>
-    );
+  return (
+    <div>
+      <h1>father</h1>
+      <Name name="dog" />
+      {props.children}
+    </div>
+  );
 }
-export {Child};
+export { Child };
