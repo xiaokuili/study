7601fa41cc1465bdd0410ab97ec2526394ae01b0
yin1991 <84140478+xiaokuili@users.noreply.github.com>
2023-03-01 06:17:50 +0000

    父子之间传递信息，由于props是只读，所以只能传递不能重新渲染

--- my-react/src/component.js
+++ my-react/src/component.js
@@ -9,14 +9,25 @@ function Child() {
 }
 
 function Name(props) {
-  return <div>name is {props.name}</div>;
+  const handle = () => {
+    props.ondata("cat");
+  };
+  return (
+    <div>
+      <button onClick={handle}>click me</button>
+      <div>name is {props.name}</div>
+    </div>
+  );
 }
 
 export default function Fathter(props) {
+  const handle = (data) => {
+    console.log(data);
+  };
   return (
     <div>
       <h1>father</h1>
-      <Name name="dog" />
+      <Name name="dog" ondata={handle} />
       {props.children}
     </div>
   );
